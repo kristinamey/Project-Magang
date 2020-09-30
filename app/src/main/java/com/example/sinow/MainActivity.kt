eb678648564b7297b4ac3543bd6a00b87ea5682a
Kristina Mey <kristinameyy@gmail.com>
2020-09-30 11:36:33 +0700

    login dan wall mey

--- app/src/main/java/com/example/sinow/MainActivity.kt
+++ app/src/main/java/com/example/sinow/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.sinow
 
+import android.app.backup.SharedPreferencesBackupHelper
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.core.view.DragStartHelper
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_main.*
