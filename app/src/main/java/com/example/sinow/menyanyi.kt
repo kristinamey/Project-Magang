db6a7a71a53291be831248add75a251b3e6c7109
Kristina Mey <kristinameyy@gmail.com>
2020-10-02 14:24:32 +0700

    permission mey

--- app/src/main/java/com/example/sinow/menyanyi.kt
+++ app/src/main/java/com/example/sinow/menyanyi.kt
@@ -3,12 +3,16 @@ package com.example.sinow
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_hiburan.*
-import kotlinx.android.synthetic.main.activity_quis.*
+import kotlinx.android.synthetic.main.activity_menyanyi.*
 
 class menyanyi : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menyanyi)
+
+        keluar3.setOnClickListener {
+            val intent = Intent(this, hiburan::class.java)
+            startActivity(intent)
+        }
     }
 }
