d7b1987346dbb3318fb4c52aebec5ea837c65357
Alex Nugraha <alexnugrahanew@gmail.com>
2020-11-19 11:12:02 +0700

    load data list huruf

--- app/src/main/java/com/example/sinow/Adaptermengenalhuruf.kt
+++ app/src/main/java/com/example/sinow/Adaptermengenalhuruf.kt
@@ -2,25 +2,43 @@ package com.example.sinow
 
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import coil.ImageLoader
+import coil.api.load
+import coil.decode.SvgDecoder
+import coil.request.LoadRequest
+import com.example.sinow.model.ModelHuruf
 import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.itemtab2mengenalhuruf.view.*
+import java.util.*
 
-class Adaptermengenalhuruf(private val onClick:(String) -> Unit)
+class Adaptermengenalhuruf(private val onClick:(ModelHuruf) -> Unit)
     : RecyclerView.Adapter<Adaptermengenalhuruf.ViewHolder>() {
 
-    var items : List<String> = emptyList()
+    var items : List<ModelHuruf> = emptyList()
         set(value) {
             field = value
             notifyDataSetChanged()
         }
-
+    private var selectedItem = 0
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(parent.inflate(R.layout.fragment_tab2mengenalhuruf))
+        return ViewHolder(parent.inflate(R.layout.itemtab2mengenalhuruf))
     }
 
     override fun onBindViewHolder(holder: Adaptermengenalhuruf.ViewHolder, position: Int) {
         val data = items[position]
+
+        holder.itemView.view.isVisible = selectedItem == position
+
         with(holder.itemView){
+            kotakmengenalhuruf.loadSvgOrOthers(data.gambar)
+            kotakmengenalhuruf.setOnClickListener {
+                onClick(items[holder.adapterPosition])
+                selectedItem = holder.adapterPosition
+                notifyDataSetChanged()
+            }
         }
     }
 
@@ -30,6 +48,25 @@ class Adaptermengenalhuruf(private val onClick:(String) -> Unit)
 
     inner class ViewHolder(override val containerView: View) :
         RecyclerView.ViewHolder(containerView), LayoutContainer {
+    }
+
+    fun ImageView.loadSvgOrOthers(myUrl: String?) {
+        myUrl?.let {
+            if (it.toLowerCase(Locale.ENGLISH).endsWith("svg")) {
+                val imageLoader = ImageLoader.Builder(this.context)
+                    .componentRegistry {
+                        add(SvgDecoder(this@loadSvgOrOthers.context))
+                    }
+                    .build()
+                val request = LoadRequest.Builder(this.context)
+                    .data(it)
+                    .target(this)
+                    .build()
+                imageLoader.execute(request)
+            } else {
+                this.load(myUrl)
+            }
+        }
     }
 
 
--- app/src/main/java/com/example/sinow/Fragment_tab2mengenalhuruf.kt
+++ app/src/main/java/com/example/sinow/Fragment_tab2mengenalhuruf.kt
@@ -1,10 +1,24 @@
 package com.example.sinow
 
+import android.app.ProgressDialog
+import android.media.MediaPlayer
+import android.net.Uri
+import android.os.AsyncTask
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.sinow.api.RequestHandler
+import com.example.sinow.model.ModelHuruf
+import com.example.sinow.model.ModelQuis
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.android.synthetic.main.activity_quismembaca.*
+import kotlinx.android.synthetic.main.fragment_tab2mengenalhuruf.*
+import org.json.JSONException
+import org.json.JSONObject
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -13,22 +27,17 @@ class Fragment_tab2mengenalhuruf : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-
-         val adapteritem:Adaptermengenalhuruf by lazy(LazyThreadSafetyMode.NONE){
-             Adaptermengenalhuruf(::tombol)
-         }
-        }
-
+    var JSON_STRING = ""
+    var next_page = ""
+    var prev_page_url = ""
+    val adapteritem:Adaptermengenalhuruf by lazy(LazyThreadSafetyMode.NONE){
+        Adaptermengenalhuruf(::tombol)
     }
-    private fun tombol(s: String) {
-        TODO("Not yet implemented")
-
+    private fun tombol(s: ModelHuruf) {
+        val media2 = MediaPlayer()
+            media2.setDataSource(requireContext(), Uri.parse(s.sound))
+            media2.prepare()
+            media2.start()
     }
 
     override fun onCreateView(
@@ -38,14 +47,55 @@ class Fragment_tab2mengenalhuruf : Fragment() {
         return inflater.inflate(R.layout.fragment_tab2mengenalhuruf, container, false)
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Fragment_tab2mengenalhuruf().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        getJSON()
+        tab2mengenalhuruf.adapter = adapteritem
+    }
+
+    private fun showEmployee() {
+        try {
+            val jsonObject = JSONObject(JSON_STRING)
+            val json = jsonObject.getString("data")
+            val list = object : TypeToken<ArrayList<ModelHuruf>>() {}.type
+            val data = Gson().fromJson<ArrayList<ModelHuruf>>(json, list)
+            adapteritem.items = data
+
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun getJSON() {
+        class GetJSON :
+            AsyncTask<Void?, Void?, String>() {
+
+            var loading: ProgressDialog? = null
+            override fun onPreExecute() {
+                super.onPreExecute()
+                loading = ProgressDialog.show(
+                    requireContext(),
+                    "Sedang memproses...",
+                    "Tunggu...",
+                    false,
+                    false
+                )
             }
+
+            override fun onPostExecute(s: String) {
+                super.onPostExecute(s)
+                loading!!.dismiss()
+                JSON_STRING = s
+                showEmployee()
+            }
+
+            override fun doInBackground(vararg params: Void?): String {
+                val rh = RequestHandler()
+                return rh.sendGetRequest("${resources.getString(R.string.base_url)}api/huruf?list=all")
+            }
+        }
+
+        val gj = GetJSON()
+        gj.execute()
     }
 }
