a53afad3ac3de3192542e0e97e68a2461e6b2e64
Alex Nugraha <alexnugrahanew@gmail.com>
2020-11-27 19:23:35 +0700

    mewarnai done

--- app/src/main/java/com/example/sinow/mewarnai.kt
+++ app/src/main/java/com/example/sinow/mewarnai.kt
@@ -1,9 +1,12 @@
 package com.example.sinow
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_menyanyi.*
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_menyanyi.keluar3
+import kotlinx.android.synthetic.main.activity_mewarnai.*
 
 class mewarnai : AppCompatActivity() {
 
@@ -15,11 +18,36 @@ class mewarnai : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mewarnai)
 
+        toolbar.title = resources.getString(R.string.app_name)
+        setSupportActionBar(toolbar)
+        
         keluar3.setOnClickListener {
             onBackPressed()
         }
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.image)
+        imageView.setImageBitmap(bitmap)
+        canvas_drawing.paintColor(resources.getColor(R.color.biru))
+        canvas_drawing.paintStork(20)
     }
     private fun tombol(Data: String){
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.ic_drawing -> canvas_drawing.paintColor(resources.getColor(R.color.colorAccent))
+            R.id.ic_eraser -> canvas_drawing.paintColor(resources.getColor(R.color.biru))
+            R.id.ic_restart -> canvas_drawing.clear()
+            else -> {
+            }
+        }
+
+        return true
     }
 }
